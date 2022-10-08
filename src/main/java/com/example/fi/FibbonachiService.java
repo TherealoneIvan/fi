4434f7b1e73b6dd8c6153e4fc2abb878a535986c
onosamo <medvedevivan5252@yandex.ru>
2022-10-08 22:38:05 +0300

    No task:_

---
+++ src/main/java/com/example/fi/FibbonachiService.java
@@ -0,0 +1,8 @@
+package com.example.fi;
+
+import com.example.fi.model.Fibbonachi;
+
+public interface FibbonachiService {
+    public Fibbonachi fib = null;
+    public int f(int n);
+}
---
+++ src/main/java/com/example/fi/model/Fibbonachi.java
@@ -0,0 +1,17 @@
+package com.example.fi.model;
+
+public class Fibbonachi {
+    private int N;
+
+    public Fibbonachi(int n) {
+        N = n;
+    }
+
+    public void setN(int n) {
+        N = n;
+    }
+
+    public int getN() {
+        return N;
+    }
+}
